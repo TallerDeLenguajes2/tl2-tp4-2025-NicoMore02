987c12035a3400f4a14a27e1ab0632d43dbaa87c
Fabricio Nicolas Moreno <nicomore053@gmail.com>
2025-09-12 20:08:46 -0300

    Get cadetes

--- Controllers/CadeteriaController.cs
+++ Controllers/CadeteriaController.cs
@@ -12,16 +12,35 @@ public class CadeteriaController : ControllerBase
 
     public CadeteriaController()
     {
+        cadeteria.AnadirCadete(1, "Carlos", "Calle Falsa 123", 11223344);
+        cadeteria.AnadirCadete(2, "Lucía", "Av. Libertad 456", 11998877);
         Cliente cliente1 = new Cliente("Fabricio", "Barrio Policial", 3818920471, "Porton negro");
         Cliente cliente2 = new Cliente("María", "Centro", 3812345678, "Casa esquina");
         cadeteria.CrearPedido("Milanesa", "De Carne", cliente1);
         cadeteria.CrearPedido("Pizza", "Napolitana", cliente2);
     }
 
-    [HttpGet]
-    public ActionResult<List<Pedido>> GetPedidos()
+    /// <summary>
+    /// Se obtiene la lista de los pedidos
+    /// </summary>
+    /// <returns>Devuelve la lista de pedidos</returns>
+    [HttpGet("pedidos")]
+    public IActionResult GetPedidos()
     {
         if (cadeteria.Pedidos is null) NotFound();
         return Ok(cadeteria.Pedidos);
     }
+
+
+    /// <summary>
+    /// Se obtiene la lista de todos los cadetes disponibles
+    /// </summary>
+    /// <returns>lista de cadetes</returns>
+    [HttpGet("cadetes")]
+    public IActionResult GetCadetes()
+    {
+        if (cadeteria.Cadetes1 is null) NotFound();
+        return Ok(cadeteria.Cadetes1);
+    }
+
 }
--- obj/Debug/net9.0/tl2-tp4-2025-NicoMore02.AssemblyInfo.cs
+++ obj/Debug/net9.0/tl2-tp4-2025-NicoMore02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp4-2025-NicoMore02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1053f9274cc573b4c659fcb636decf5402ab967")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp4-2025-NicoMore02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp4-2025-NicoMore02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
